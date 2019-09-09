e16e92cf66658b4e0bba2d96aa6cb8f23e08fd7b
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2019-09-09 16:10:23 +0700

    update fformat from files

--- Day1/handle_array.py
+++ Day1/handle_array.py
@@ -1,26 +1,26 @@
 import numpy as np
 
-a=np.array([0,1,2,3,4,5])
+a = np.array([0, 1, 2, 3, 4, 5])
 
 print(a)
 
-#Hien thi so chieu cua a o dang ma tran
+# Hien thi so chieu cua a o dang ma tran
 print(a.ndim)
 
 print(a.shape)
 
-#Hien thi phan tu lon hon 3
-print(a[a>3])
+# Hien thi phan tu lon hon 3
+print(a[a > 3])
 
-#Thay doi gia tri
-a[a>3] = 10
+# Thay doi gia tri
+a[a > 3] = 10
 
-b=a.reshape((3,2))
+b = a.reshape((3, 2))
 
 print(b)
 
 print(b[2][1])
 
-c=b*2
+c = b*2
 
 print(c)
--- Day2/iris.py
+++ Day2/iris.py
@@ -1,31 +1,33 @@
-#Lay file iris truc tiep tu sklearn
+# Lay file iris truc tiep tu sklearn
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_iris
 iris_dt = load_iris()
-iris_dt.data[1:5] # thuoc tinh tap cua iris_dt
-iris_dt.target[1:5] # gia tri cua nhan / class
+iris_dt.data[1:5]  # thuoc tinh tap cua iris_dt
+iris_dt.target[1:5]  # gia tri cua nhan / class
 
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(iris_dt.data, iris_dt.target, test_size=1/3.0, random_state=123)
+X_train, X_test, y_train, y_test = train_test_split(
+    iris_dt.data, iris_dt.target, test_size=1/3.0, random_state=123)
 
 X_train[1:6]
-X_train[1:6,1:3]
+X_train[1:6, 1:3]
 y_train[1:6]
 X_test[6:10]
 y_test[6:10]
 
-#Xay dung mo hinh cay quyet dinh du tren chi so Gini
-from sklearn.tree import DecisionTreeClassifier
-clf_gini = DecisionTreeClassifier(criterion = "entropy", random_state = 100, max_depth = 5, min_samples_leaf=None)
+# Xay dung mo hinh cay quyet dinh du tren chi so Gini
+clf_gini = DecisionTreeClassifier(
+    criterion="entropy", random_state=100, max_depth=5, min_samples_leaf=None)
 clf_gini.fit(X_train, y_train)
 
-#du doan
+# du doan
 y_pred = clf_gini.predict(X_test)
 y_test
-clf_gini.predict([[4,4,3,3]])
+clf_gini.predict([[4, 4, 3, 3]])
 
-#tinh do chinh xac
-from sklearn.metrics import accuracy_score
+# tinh do chinh xac
 print("Accuracy is", accuracy_score(y_test, y_pred)*100)
 
-from sklearn.metrics import confusion_matrix
-confusion_matrix(y_test, y_pred, labels=[2,0,1])
+confusion_matrix(y_test, y_pred, labels=[2, 0, 1])
